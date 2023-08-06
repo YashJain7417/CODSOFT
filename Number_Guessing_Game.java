06e9095a13e1a868e339b143dd2bd9a0bbc75bfa
YashJain7417 <138286685+YashJain7417@users.noreply.github.com>
2023-08-06 09:28:54 -0700

    Number_Guessing_Game.java
    A number guessing game by using JAVA , which is working in terminal. And user can play 3 rounds with 10turns each round.
    It also shows the highscore at the end from all the three rounds.

--- Number_Guessing_Game.java
+++ Number_Guessing_Game.java
@@ -1,40 +1,57 @@
+/* TASK 1 - 𝗡𝗨𝗠𝗕𝗘𝗥 𝗚𝗔𝗠𝗘
+
+1. Generate a random number within a specified range, such as 1 to 100.
+
+2. Prompt the user to enter their guess for the generated number.
+
+3. Compare the user's guess with the generated number and provide feedback on whether the guess
+is correct, too high, or too low.
+
+4. Repeat steps 2 and 3 until the user guesses the correct number.
+
+You can incorporate additional details as follows:
+
+5. Limit the number of attempts the user has to guess the number.
+6. Add the option for multiple rounds, allowing the user to play again.
+7. Display the user's score, which can be based on the number of attempts taken or rounds won. */
+
 import java.util.Random;
 import java.util.Scanner;
 
 public class Number_Guessing_Game {
     public static void main(String[] args) {
-        int trounds = 3;
+        int trounds = 3; //Total Rounds
         System.out.println("Guess a Number between 1 to 100.");
         System.out.println("You will have 10 turns to guess the right Number !");
         System.out.println("All The Best :)");
         Scanner sc = new Scanner(System.in);
-        int hscore = 0;
-        for (int round = 1; round <= trounds; round++) {
+        int hscore = 0; // Highscore
+        for (int round = 1; round <= trounds; round++) { //loop for 3 rounds
             Random random_num = new Random();
             int rguess = random_num.nextInt(100);
-            int turns = 0;
-            int guess;
+            int turns = 0; //number of turns
+            int guess; //user input number
             int i = 0;
             Boolean wins = false;
             while (wins == false) {
                 guess = sc.nextInt();
                 turns++;
-                if (guess > 100) {
+                if (guess > 100) {//if number is greater than 100
                     System.out.println("Number is out of range. Please choose a number between 1 to 100 only.");
                 }
                 if (guess == rguess) {
-                    wins = true;
-                } else if (i > 8) {
+                    wins = true; //you wins
+                } else if (i > 8) { //you lost
                     System.out.println(" ");
                     System.out.println("You LOST!!!!");
                     System.out.println("You have lost all your turns. The Right Answer was: " + rguess);
                     System.out.println("Your Score Is 0 :(");
-                } else if (guess < rguess) {
+                } else if (guess < rguess) { //Too LOw
                     i++;
                     System.out.println("Too Low ! Your Guess Number is Lower than the Right Number.");
                     System.out.println("You Have " + (10 - turns) + " Turns Left !");
-                } else if (guess > rguess) {
-                    i++;
+                } else if (guess > rguess) { //too high
+                    i++; 
                     System.out.println("Too High ! Your Guess Number is Higher than the Right Number.");
                     System.out.println("You Have " + (10 - turns) + " Turns Left !");
                 }
@@ -68,3 +85,6 @@ public class Number_Guessing_Game {
         sc.close();
     }
 }
+
+
+
